d4be6fa9280c2528c70de57f9d4cdbfe440278c2
Hannes Blåman <hannes@blaman.com>
2026-02-19 22:37:49 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const CHECKBOXES = document.querySelectorAll('#list input[type=checkbox]')
+const SUBMIT = document.getElementById('submit');
+const RESET = document.getElementById('reset');
+
+SUBMIT.addEventListener('click', function() {
+	let score = CHECKBOXES.length;
+	for (let box of CHECKBOXES) {
+		if (box.checked) score--;
+	}
+	window.location.href = `/score.html?score=${score}`;
+});
+
+RESET.addEventListener('click', function() {
+	for (let box of CHECKBOXES) {
+		box.checked = false;
+	}
+});
